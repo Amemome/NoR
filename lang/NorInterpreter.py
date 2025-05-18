77d86a41e11520b06e5aafee83d265040775ad89
Amemome <meltksq5140@gmail.com>
2025-05-19 00:11:07 +0900

    [LANG] feat: 기본 AST생성 함수추가

---
+++ lang/NorInterpreter.py
@@ -0,0 +1,10 @@
+from lark import Lark, Transformer, v_args, Token
+
+VALID_GRAPH_TYPE = ["",]
+
+class SemanticAnalyzer:
+    VALID_GRAPH_TYPE = ["bar", "line", "scatter"]
+
+
+
+class NorInterpreter(Transformer):
--- lang/main.py
+++ lang/main.py
@@ -1,4 +1,5 @@
 from lark import Lark
+from lark.exceptions import LarkError, UnexpectedToken, UnexpectedCharacters, UnexpectedEOF
 
 grammar_file_path = 'nor.lark'
 grammar = None
@@ -8,19 +9,29 @@ with open(grammar_file_path, 'r', encoding='utf-8') as f:
 
 parser = Lark(grammar)
 
-tree = parser.parse("10")
-print(tree.pretty())
+parse_tree = None
 
-tree = parser.parse("10 + 20 + 30 + 40")
-print(tree.pretty())
+script = """
+        그래프생성 "하이"
+        그려줘
+        //this is line comment
+        데이터는 [1,2,3,4]
+        """
+try:
+    parse_tree = parser.parse(script)
+    print(parse_tree.pretty())
+    
+except UnexpectedCharacters as err:
+    print(f"파싱 오류 {UnexpectedCharacters}: 행 {err.line}, 열 {err.column}, 토큰 '{err.token}'")
+    print(f"  예상 토큰: {err.expected}")
+    print(f"  컨텍스트: {err.get_context(script, 20)}")
+except UnexpectedCharacters as err:
+    print(f"파싱 오류 (UnexpectedCharacters): 행 {err.line}, 열 {err.column}, 문자 '{err.char}'")
+    print(f"  컨텍스트: {err.get_context(script, 20)}")
+except UnexpectedEOF as err:
+    print(f"파싱 오류 (UnexpectedEOF): 입력이 예기치 않게 종료되었습니다.")
+    print(f"  예상 토큰: {err.expected}")
+except LarkError as le: # 기타 Lark 파싱 오류
+    print(f"파싱 오류: {le}")
 
-tree = parser.parse("""123
-                    //this is line comment
-                    10 + 20 + 30
-                    /* this is block 
-                    co
-                    mm
-                    ent
-                    */
-                    """)
-print(tree.pretty())
+    
