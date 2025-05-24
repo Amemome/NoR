f4bc2b897a489fabdd57899ec7bd6b73e58a1d55
Amemome <meltksq5140@gmail.com>
2025-05-24 23:05:33 +0900

    [LANG] feat: 실행기에 문장정의

--- lang/executor.py
+++ lang/executor.py
@@ -1,10 +1,13 @@
 from lark import Transformer, v_args
+from error import RunningError
 
 class Executor(Transformer):
     def __init__(self):
-        super.__init__()
+        super().__init__()
         self.current_graph = None # 현재 가리키고 있는 그래프 객체
         self.graph_context = dict()
+
+        
 
     def _reset_current_graph(self, name="제목 없는 그래프"):
         self.graph_data = {
@@ -22,7 +25,19 @@ class Executor(Transformer):
                 'output': {}
             }
         }       
-    
+
+    def _get_current_graph_options(self):
+        """현재 그래프 컨텍스트에 있는 데이터들 반환"""
+        if self.current_graph and self.current_graph in self.graph_context:
+            return self.graph_context[self.current_graph]
+        self._add_error("그래프가 컨텍스트에 없습니다")
+
+    def _add_error(self, token=None, message="메세지가 없습니다"):
+        line = getattr(token, 'line', '?')
+        column = getattr(token, 'column', '?')
+
+        self.errors.append(RunningError(line, column, message)) 
+
     @v_args(inline=True)
     def atom(self, value_token):
         if value_token.type == 'NUMBER':
@@ -56,10 +71,8 @@ class Executor(Transformer):
     
     def create_graph_statement(self, items):
         graph_name = items[1].value.strip("'\"")
-        self._reset_current_graph(graph_name)
+        self.graph_context[graph_name] = self._reset_current_graph(graph_name)
         self.current_graph = graph_name
-
-    def 
 
     
     def data_statement(self, items):
@@ -67,6 +80,36 @@ class Executor(Transformer):
         data = items[2]
         if not self.graph_data['type']:
             print("error.")
+
+    def object_selector(self, items):
+        return items[0].type
+
+    def property_key(self, items):
+        return items[0]
+    
+    def property_assignment_statement(self, items): 
+        current_graph_data = self._get_current_graph_options()
+
+
+
+
+    def draw_statement(self, items):
+        current_graph_to_draw = self._get_current_graph_options()
+
+        if current_graph_to_draw:
+            # 실제 함수 호출
+            pass
+        else:
+            self._add_error("그래프 그릴 수 없는 상황")
+
+    def set_axis_labels_statement(self, items):
+        pass
+
+    def load_command(self, items):
+        pass
+
+    def save_command(self, items):
+        pass
 
 
 
