772fbe643d78471dbcdbb01af43c5eac83265960
Amemome <meltksq5140@gmail.com>
2025-05-24 16:44:24 +0900

    [LANG] feat: nor_config 설명 추가

--- lang/nor_config.py
+++ lang/nor_config.py
@@ -1,10 +1,12 @@
-# nor_config.py
+# nor의 설정들을 관리하는 파일입니다.
+
+
+# 각 속성들에 대해서 가능한 값들을 STRING으로 관리합니다.
 
 COLOR_VALUES = {
     "빨강", "파랑", "초록", "검정", "하양", "노랑", "보라", "분홍",
     "red", "blue", "green", "black", "white", "yellow", "purple", "pink"
 }
-
 
 MARKER_SHAPES = {"원", "사각형", "점", "엑스", "o", "s", ".", "x", "삼각형", "별"} # 마커 문양
 
@@ -17,36 +19,41 @@ LEGEND_POSITIONS = {
     "center left", "center right", "lower center", "upper center", "center"
 }
 
+# 가능한 값 집합을 통해서 딕셔너리를 구성합니다.
+
 VALIDATION_RULES = {
+    # "그래프" 속성 지정자
     "GRAPH_KEYWORD": {
         "SET_TYPE_KEYWORD": GRAPH_TYPES,
         "SET_TITLE_KEYWORD": None,
         "SET_BACKGROUND_KEYWORD": COLOR_VALUES,
         "SET_LEGEND_KEYWORD": LEGEND_POSITIONS, 
-        # ... (SIZE_KEYWORD, SET_COLOR_KEYWORD 등 일반 속성이 그래프 자체에 적용될 때)
+        
     },
+    # "마커" 속성 지정자
     "MARKER_KEYWORD": {
-        "SET_TYPE_KEYWORD": MARKER_SHAPES, # 마커의 "종류" 또는 "문양"
-        "SIZE_KEYWORD": None, # 마커의 "크기"
-        "SET_COLOR_KEYWORD": COLOR_VALUES, # 마커의 "색"
-        # "SET_BORDER_COLOR_KEYWORD": COLOR_VALUES (새로운 EBNF 터미널 필요)
+        "SET_TYPE_KEYWORD": MARKER_SHAPES,
+        "SIZE_KEYWORD": None, 
+        "SET_COLOR_KEYWORD": COLOR_VALUES, 
     },
+    # "선" 속성 지정자
     "LINE_KEYWORD": {
-        "SET_TYPE_KEYWORD": LINE_STYLES, # 선의 "종류" 또는 "스타일"
-        "SET_THICKNESS_KEYWORD": None, # 선의 "굵기"
-        "SET_COLOR_KEYWORD": COLOR_VALUES, # 선의 "색"
-        # "SET_ALPHA_KEYWORD": None (새로운 EBNF 터미널 필요)
-    },
-    "X_AXIS_KEYWORD": {
-        "SET_NAME_KEYWORD": None, # 축 "이름" (또는 SET_LABELS_KEYWORD와 연동)
-        "SET_COLOR_KEYWORD": COLOR_VALUES, # 축 "색"
-        "SIZE_KEYWORD": None, # 축 글꼴 "크기"
+        "SET_TYPE_KEYWORD": LINE_STYLES, 
+        "SET_THICKNESS_KEYWORD": None,
+        "SET_COLOR_KEYWORD": COLOR_VALUES,
 
     },
+    # "X축" 속성 지정자
+    "X_AXIS_KEYWORD": {
+        "SET_NAME_KEYWORD": None, 
+        "SET_COLOR_KEYWORD": COLOR_VALUES, 
+        "SIZE_KEYWORD": None,
+
+    },
+    # "Y축" 속성 지정자
     "Y_AXIS_KEYWORD": {
         "SET_NAME_KEYWORD": None,
         "SET_COLOR_KEYWORD": COLOR_VALUES,
         "SIZE_KEYWORD": None,
-       
     },
 }
