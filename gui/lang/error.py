d6762bda16e27696adcdc2f3047a6d3e212ca7f0
amemome <152237136+Amemome@users.noreply.github.com>
2025-06-03 12:38:28 +0900

    [LANG] feat: 에러 처리에 NoRLOG 객체 추가

--- gui/lang/error.py
+++ gui/lang/error.py
@@ -1,23 +1,18 @@
-class Error():
-    def __init__(self, line, column):
-        self.line = line
-        self.column = column
-
-    def __str__(self):
-        return f"에러 ({self.line}번째 줄 {self.column}번째): "
+from .nor_log import NorLog
     
-class CompileError(Error):
+class CompileError(NorLog):
+    """의미 분석 단계에서 발생하는 에러 (컴파일 에러)."""
     def __init__(self, line, column, message):
-        super().__init__(line, column)
-        self.message = message
+        super().__init__(source="의미 분석기", type="error", message=message, line=line, column=column)
 
     def __str__(self):
-        return f"컴파일" + super().__str__() + self.message
+        return super().__str__()
 
-class RunningError(Error):
-    def __init__(self, line, column, message):
-        super().__init__(line, column)
-        self.message = message
+class RunningError(NorLog):
+    """실행 단계에서 발생하는 에러 (런타임 에러)."""
+    def __init__(self, line: int, column: int, message: str):
+        # source를 "executor"로 고정하고 type을 "error"로 고정합니다.
+        super().__init__(source="executor", type="error", message=message, line=line, column=column)
 
     def __str__(self):
-        return f"런타임" + super().__str__() + self.message
+        return super().__str__() # NorLog의 __str__을 따름
